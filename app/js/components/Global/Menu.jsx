dff4eca689125c7cd7c2599cf714239d94146df3
Anup <9003435@ulta.com>
2017-09-18 16:29:50 -0500

    added the git ignore

--- app/js/components/Global/Menu.jsx
+++ app/js/components/Global/Menu.jsx
@@ -4,7 +4,7 @@ class Menu extends Component {
   render() {
     return (
       <div className='Menu'>
-        Main Menu Anup Sharan Ambula
+        Main Menu Anup Sharan Ambula PPPPP
       </div>
     );
   }
